862d3d41dc57ccb804048679ef402e6d160bd6f0
Dlray89 <dlrayjr89@gmail.com>
2020-02-11 01:17:21 -0500

    David-Ray

--- src/App.js
+++ src/App.js
@@ -1,89 +1,18 @@
-import React from 'react';
-import ToDoList from "./components/TodoComponents/TodoList";
+import React from "react";
 import ToDoForm from "./components/TodoComponents/TodoForm";
 
 
-const toDo = [
-  {
-    task: "Do react project",
-    id: 10,
-    completed: false 
-  },
-  {
-    task: "Do react project",
-    id: 20,
-    completed: false 
-  },
-  {
-    task: "Do react project",
-    id: 30,
-    completed: false 
-  },
-  {
-    task: "Do react project",
-    id: 40,
-    completed: false 
-  },
-]
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      toDoList: toDo
-    }
-
-  }
-
-  toggleTask = clickedId => {
-
-    const newToDoList = this.state.toDoList.map(todo => {
-
-      if( todo.id === clickedId ) {
-
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-
-      }    else {
-        return todo
-      }
-    })
-
-    this.setState({
-      toDoList: newToDoList
-    })
-  };
-
-  addNewtask = itemTask => {
-    
-    const newTask = {
-      task: itemTask,
-      id: Date.now(),
-      completed: false
-    };
-
-    this.setState({
-      toDoList: [...this.state.toDoList, newTask]
-    });
-  };
-
-
+class App extends React.Component{
   render() {
-    console.log('rendering....')
-    return (
-      <div className='app'>
-        <div className='header'>
-        <h2>To do list</h2>
-        <ToDoForm  addNewtask={this.addNewtask}/>
-        </div>
-        <ToDoList 
-        toDo={this.state.toDoList}
-        toggleTask={this.toggleTask} />
-      </div>
-    );
+    return(
+    <div>
+    <h1>To do List</h1>
+    <div><ToDoForm />
+    </div>
+   
+    </div>
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,40 +1,83 @@
-import React, { Component } from "react";
+import React from "react";
+import ListItems from "./ListItems";
 
-class ToDoForm extends Component {
-    constructor() {
-        super();
+
+class ToDoForm extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            newTask: ''
-        };
+            items: [],
+            currentItem: {
+                text: '',
+                key: ''
+            }
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.addItem = this.addItem.bind(this)
+        this.deleteItems = this.deleteItems.bind(this)
+        this.setUpdate = this.setUpdate.bind(this)
     }
 
-    handleChanges = event => {
-        this.setState({
-            newTask: event.target.value
-        });
-    };
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.addNewtask(this.state.newtask);
-        this.setState({ newTask: '' });
+    handleInput(e){
+        this.setState({ 
+            currentItem:{
+                text: e.target.value,
+                key: Date.now()
+            }
+        })
     }
 
+    addItem(e) {
+        e.preventDefault();
+
+        const newItem = this.state.currentItem;
+
+        if(newItem.text !=='') {
+            const newItems = [...this.state.items, newItem]
+            this.setState({
+                items: newItems,
+                currentItem: {
+                    text: '',
+                    key: ''
+                }
+            })
+        }
+    }
+
+    deleteItems(key){
+        const fliterItems = this.state.items.filter(item => 
+            item.key !== key);
+            this.setState({
+                items: fliterItems
+            })
+           
+    }
+
+    setUpdate(text, key){
+        const items = this.state.items;
+        items.map( item => {
+            if(item.key === key){
+                item.text = text;
+                this.setState({
+                    items: items
+                })
+            }
+        })
+    }
     render() {
-        console.log('render form');
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type='text'
-                    name='newTask'
-                    value={this.state.newTask}
-                    onChange={this.handleChanges}
-                />
-                <button> Add Task </button>
-            </form>
+            <div>
+            <header>
+                <form onSubmit={this.addItem}>
+                    <input  type="text" placeholder="Add task" value={this.state.currentItem.Text} onChange={this.handleInput}/>
+                    <button type="submit"> Add</button>
+                </form>
+                <ListItems items={this.state.items} deleteItems={this.deleteItems} setUpdate={this.setUpdate}></ListItems>
+
+            </header>
+            </div>
         )
-
     }
-
 }
 
 export default ToDoForm;
